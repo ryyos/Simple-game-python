e1db91bf8b9f71b7469b1719335c91ca6f4a2da1
ryosyntaxx <riodwi12174@gmail.com>
2023-09-09 22:28:59 +0700

    change modul equipment

--- blacksmite.py
+++ blacksmite.py
@@ -1,7 +1,6 @@
-from equipment import weapon_switch
-from equipment import armor_switch
-from equipment import fundamental
+from equipment import user
 from class_prog import Hero
+from computer import computer
 
 
 class Armor:
@@ -18,11 +17,14 @@ chose = int(input('Input Your Choise : '))
 match chose:
     case 1:
         alexa = Hero('Alexa', 100, 17, 10, 50, 50)
-        fundamental(chose, alexa)
+        user.fundamental(chose, alexa)
     case 2:
         zoldyck = Hero('Zoldyck', 120, 15, 7, 50, 50)
-        fundamental(chose, zoldyck)
+        user.fundamental(chose, zoldyck)
 
     case 3:
         lux = Hero('Lux', 90, 20, 10, 60, 30)
-        fundamental(chose, lux)
+        user.fundamental(chose, lux)
+
+print(f'Computer turn..')
+computer()
