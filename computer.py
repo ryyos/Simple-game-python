8888f2c511be744d7ec8200f7ed0454f0031bb17
ryosyntaxx <riodwi12174@gmail.com>
2023-09-10 19:19:05 +0700

    change to return function

--- computer.py
+++ computer.py
@@ -9,11 +9,13 @@ def computer():
     match chose:
         case 1:
             alexa = Hero('Alexa', 100, 17, 10, 50, 50)
-            comp.fundamental(chose, alexa)
+            comp_yusha = comp.fundamental(chose, alexa)
         case 2:
             zoldyck = Hero('Zoldyck', 120, 15, 7, 50, 50)
-            comp.fundamental(chose, zoldyck)
+            comp_yusha = comp.fundamental(chose, zoldyck)
 
         case 3:
             lux = Hero('Lux', 90, 20, 10, 60, 30)
-            comp.fundamental(chose, lux)
+            comp_yusha = comp.fundamental(chose, lux)
+
+    return comp_yusha
