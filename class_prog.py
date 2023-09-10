e97db6f38caeb648b49dbb5175de6845a0cb3b8e
ryosyntaxx <riodwi12174@gmail.com>
2023-09-10 19:18:13 +0700

    add status bar

--- class_prog.py
+++ class_prog.py
@@ -2,6 +2,7 @@ class Hero:
 
     jum_hero = 0
     all_hero = []
+    # health = 0
 
     def __init__(self, name, health, damage, defend, mana, energy):
         self.name = name
@@ -12,9 +13,7 @@ class Hero:
         self.energy = energy
         Hero.all_hero.append(name)
         Hero.jum_hero += 1
-
-    # def health_up(self, plus):
-    #     self.health += plus
+        # Hero.health += health
 
     def damage_up(self, plus):
         self.damage += plus
@@ -22,3 +21,17 @@ class Hero:
     def defend_up(self, health, armor):
         self.defend += armor
         self.health += health
+
+    def attack(self, enemy):
+        enemy.attacked(self)
+        print(f'Your attack {enemy.name}')
+        print('Status :')
+        print(f'Damage dault : {self.damage - enemy.defend}')
+        print(f'Your energy  : {self.energy}')
+        print(f'Your mana    : {self.mana}')
+        print(f'Enemy health remaining : {enemy.health}')
+
+    def attacked(self, enemy):
+        print(f'{self.name} attacked {enemy.name}')
+        self.health -= (enemy.damage - self.defend)
+        print(f'Your Health Remaining : {self.health}')
